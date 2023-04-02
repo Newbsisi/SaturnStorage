1bf047440aecd746d7b5119da1617e4ca3db993b
lucasfrederiksen <lsfr21@student.aau.dk>
2023-04-02 13:28:39 +0200

    new design for Homepage

--- Homepage.java
+++ Homepage.java
@@ -1,13 +1,43 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import javax.swing.*;
+
 public class Homepage {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         JFrame a = new JFrame("SaturnStorage");
-        JTextField b = new JTextField("Homepage");
-        b.setBounds(50,100,200,30);
-        a.add(b);
-        a.setSize(300,300);
-        a.setLayout(null);
+        JTextField b = new JTextField("Search");
+
+        JButton cameraB = new JButton("Camera");
+        JButton lightB = new JButton("Light");
+        JButton cablesB = new JButton("Cables");
+        JButton propsB = new JButton("Props");
+        JButton camerastandsB = new JButton("Stands");
+        JButton microphoneB = new JButton("Microphone");
+
+        JPanel buttonPanel = new JPanel(new GridLayout(6, 1));
+        buttonPanel.add(cameraB);
+        buttonPanel.add(lightB);
+        buttonPanel.add(cablesB);
+        buttonPanel.add(propsB);
+        buttonPanel.add(camerastandsB);
+        buttonPanel.add(microphoneB);
+
+        cameraB.setPreferredSize(new Dimension(200, 30));
+        lightB.setPreferredSize(new Dimension(200, 30));
+        cablesB.setPreferredSize(new Dimension(200, 30));
+        propsB.setPreferredSize(new Dimension(200, 30));
+        camerastandsB.setPreferredSize(new Dimension(200, 30));
+        microphoneB.setPreferredSize(new Dimension(200, 30));
+
+        JPanel searchPanel = new JPanel(new BorderLayout());
+        searchPanel.add(b, BorderLayout.NORTH);
+        searchPanel.add(buttonPanel, BorderLayout.CENTER);
+
+        a.add(searchPanel);
+
+        a.setSize(600, 400);
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         a.setVisible(true);
     }
-
 }
