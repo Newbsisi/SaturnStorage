12637e508adf01ca95ce42163f3cda1fe2232c62
Søren L <93222211+Newbsisi@users.noreply.github.com>
2023-04-24 09:05:46 +0200

    Testing

---
+++ HomePage.java
@@ -0,0 +1,69 @@
+public class Homepage {
+    public static void main(String[] args) {
+        JFrame a = new JFrame("SaturnStorage");
+        JTextField b = new JTextField("Search");
+        b.setPreferredSize(new Dimension(250,30));
+        JButton searchButton = new JButton("Search");
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                IsValidSearchString.InputValidator.validateInput(b);
+            }
+        });
+
+        b.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent searching) {
+                if (searching.getKeyCode() == KeyEvent.VK_ENTER) {
+                    IsValidSearchString.InputValidator.validateInput(b);
+                }
+            }
+        });
+
+
+
+        JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        searchPanel.add(b);
+        searchPanel.add(searchButton);
+
+        JButton cameraB = new JButton("Camera");
+        JButton lightB = new JButton("Light");
+        JButton cablesB = new JButton("Cables");
+        JButton propsB = new JButton("Props");
+        JButton camerastandsB = new JButton("Stands");
+        JButton microphoneB = new JButton("Microphone");
+
+        JPanel buttonPanel = new JPanel(new GridLayout(6, 1));
+        buttonPanel.add(cameraB);
+        buttonPanel.add(lightB);
+        buttonPanel.add(cablesB);
+        buttonPanel.add(propsB);
+        buttonPanel.add(camerastandsB);
+        buttonPanel.add(microphoneB);
+
+        cameraB.setPreferredSize(new Dimension(200, 30));
+        lightB.setPreferredSize(new Dimension(200, 30));
+        cablesB.setPreferredSize(new Dimension(200, 30));
+        propsB.setPreferredSize(new Dimension(200, 30));
+        camerastandsB.setPreferredSize(new Dimension(200, 30));
+        microphoneB.setPreferredSize(new Dimension(200, 30));
+
+        JLabel usernameLabel = new JLabel("Username");
+        JButton returnButton = new JButton("Return");
+
+        JPanel userPanel = new JPanel(new BorderLayout());
+        userPanel.add(usernameLabel, BorderLayout.WEST);
+        userPanel.add(returnButton, BorderLayout.EAST);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(searchPanel, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
+        mainPanel.add(userPanel, BorderLayout.SOUTH);
+
+        a.add(mainPanel);
+
+        a.setSize(800, 500);
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        a.setVisible(true);
+    }
+}
