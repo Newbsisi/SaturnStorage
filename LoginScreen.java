50647683d8cf2c79b4a7dbd5de98a7a996bfc757
Søren L <93222211+Newbsisi@users.noreply.github.com>
2023-04-24 07:09:09 +0200

    s

--- LoginScreen.java
+++ LoginScreen.java
@@ -55,8 +55,7 @@ public class LoginScreen extends JFrame implements ActionListener {
                 if (result.next()) {
                     JOptionPane.showMessageDialog(this, "Login successful!");
                     request.getSession().setAttribute("loggedIn", true);
-                    RequestDispatcher dispatcher = request.getRequestDispatcher("/Homepage");
-                    dispatcher.forward(request, response);
+                    response.sendRedirect("Homepage");
                 } else {
                     JOptionPane.showMessageDialog(this, "Login was not successful!");
                 }
@@ -70,5 +69,79 @@ public class LoginScreen extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         new LoginScreen();
+    }
+}
+
+public class Homepage {
+    if (request.getSession().getAttribute("loggedIn") == null) {
+        response.sendRedirect("LoginScreen");
+        return;
+    }
+    public static void main(String[] args) {
+        JFrame a = new JFrame("SaturnStorage");
+        JTextField b = new JTextField("Search");
+        b.setPreferredSize(new Dimension(250,30));
+        JButton searchButton = new JButton("Search");
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                IsValidSearchString.InputValidator.validateInput(b);
+            }
+        });
+
+        b.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent searching) {
+                if (searching.getKeyCode() == KeyEvent.VK_ENTER) {
+                    IsValidSearchString.InputValidator.validateInput(b);
+                }
+            }
+        });
+
+
+
+        JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        searchPanel.add(b);
+        searchPanel.add(searchButton);
+
+        JButton cameraB = new JButton("Camera");
+        JButton lightB = new JButton("Light");
+        JButton cablesB = new JButton("Cables");
+        JButton propsB = new JButton("Props");
+        JButton camerastandsB = new JButton("Stands");
+        JButton microphoneB = new JButton("Microphone");
+
+        JPanel buttonPanel = new JPanel(new GridLayout(6, 1));
+        buttonPanel.add(cameraB);
+        buttonPanel.add(lightB);
+        buttonPanel.add(cablesB);
+        buttonPanel.add(propsB);
+        buttonPanel.add(camerastandsB);
+        buttonPanel.add(microphoneB);
+
+        cameraB.setPreferredSize(new Dimension(200, 30));
+        lightB.setPreferredSize(new Dimension(200, 30));
+        cablesB.setPreferredSize(new Dimension(200, 30));
+        propsB.setPreferredSize(new Dimension(200, 30));
+        camerastandsB.setPreferredSize(new Dimension(200, 30));
+        microphoneB.setPreferredSize(new Dimension(200, 30));
+
+        JLabel usernameLabel = new JLabel("Username");
+        JButton returnButton = new JButton("Return");
+
+        JPanel userPanel = new JPanel(new BorderLayout());
+        userPanel.add(usernameLabel, BorderLayout.WEST);
+        userPanel.add(returnButton, BorderLayout.EAST);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(searchPanel, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
+        mainPanel.add(userPanel, BorderLayout.SOUTH);
+
+        a.add(mainPanel);
+
+        a.setSize(800, 500);
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        a.setVisible(true);
     }
 }
