d4b38bd53f8aef8dfd153c0300ed3db521809213
Søren L <93222211+Newbsisi@users.noreply.github.com>
2023-05-08 10:28:45 +0200

    whatever

--- SaltHashing.java
+++ SaltHashing.java
@@ -1,33 +1,44 @@
-import java.awt.*;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 import java.sql.*;
-import javax.swing.*;
-import java.awt.event.*;
+import java.lang.Object;
 
-public class SaltHashing {
+public class SaltHashing<random> {
+
     String username = "saltuser";
     String password = "Password3?";
     SecureRandom random = new SecureRandom();
-    
-    byte[] salt = new byte[16];
-    random.nextBytes(salt);
-    
-    MessageDigest md = MessageDigest.getInstance("SHA-256");
-    md.update(salt);
-    
-    byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
-    
+
+    byte[] salt;
+    byte[] hashedPassword;
+
+    MessageDigest md;
+
+    public SaltHashing() {
+        salt = new byte[16];
+        random.nextBytes(salt);
+
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            md.update(salt);
+            hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void insertUser() {
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/users", "user_auth", "password");
-             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO login_info (username, salt, hashed_password) VALUES (?, ?, ?)")) {
+                PreparedStatement pstmt = conn.prepareStatement("INSERT INTO login_info (username, salt, hashed_password) VALUES (?, ?, ?)")) {
             pstmt.setString(1, username);
-            pstmt.setBytes(2, salt);
+            pstmt.setString(2, DatatypeConverter.printHexBinary(salt));
             pstmt.setBytes(3, hashedPassword);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
+
     public static void main(String[] args) {
         SaltHashing saltHashing = new SaltHashing();
         saltHashing.insertUser();
